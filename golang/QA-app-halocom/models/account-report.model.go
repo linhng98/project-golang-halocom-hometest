a4546ef91dc6a1706972b0b405fed3aeef2aae8e
nobabykill <linh1612340@gmail.com>
2020-05-20 20:55:04 +0700

    add account-report model

---
+++ golang/QA-app-halocom/models/account-report.model.go
@@ -0,0 +1,8 @@
+package models
+
+// AccountReport model export
+type AccountReport struct {
+	ID uint `json:"id" gorm:"primary_key"`
+	AccountID uint `json:"account_id"`
+	ReactID uint `json:"react_id"`
+}
